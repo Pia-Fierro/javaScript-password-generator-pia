2486f9f75c105ca0a8c7dc8f7c8b3896c0385da9
pia fierro <piafierro.3187@gmail.com>
2022-11-28 15:41:22 +1000

    confim method added to script

--- script.js
+++ script.js
@@ -6,16 +6,20 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword () {
 
-//* prompt method will display information to the user to decide about the characteristics of the password
+  //* prompt method will display information to the user to decide about the lenght of the password
+
   var userLenght = window.prompt ("Between 8 and 128, how many character your password needs to have?");
   var passwordLenght = (userLenght);
+
   console.log (passwordLenght);
-    if (passwordLenght <8) {
-      window.alert ("Password must have 8 characters minimun");
-    } else if (passwordLenght >128) {
-      window.alert("password must have 128 character or less") 
-    } else {
-      var upperCaseCharacthers = window.confirm ("do you want to include upper case in your password?");
+
+    if (passwordLenght <8 || passwordLenght >128) {
+      window.alert ("Password must have minimum of 8 and maximum of 128 characters, please try again");}
+
+   //* confirm method will display acept or cancel to the user so they can decide about what carachters to include in the password 
+
+     else {
+      var upperCaseCharacthers = window.confirm ("do you want to include upper case in your password?"); 
       var lowerCaseCharacters = window.confirm ("do you want to include lower case in your password?");
       var specialCharacters = window.confirm ("do you want to include special characthers in your password?");
       var numbersCharacters = window.confirm ("do you want to include numbers in your password?");
@@ -28,16 +32,34 @@ function generatePassword () {
       
       //* if user does not accepted any of the special critaria then a numeric pasword will be generated randomly 
 
-      if (!upperCaseCharacthers && !lowerCaseCharacters && !specialCharacters && !numbersCharacters) 
-        {window.alert ("as none options have been selected, default numeric password will be created");
+      if (!upperCaseCharacthers && !lowerCaseCharacters && !specialCharacters && !numbersCharacters) {
+        window.alert ("as none options have been selected, default numeric password will be created");
+
         var options = numbers;
         var generatePassword= "";
-        
         for (var i=0;i<passwordLenght;i++){
           var randomCharacter = options[Math.floor(Math.random()*options.length)];
           console.log(randomCharacter);
           generatePassword=generatePassword+randomCharacter;
           console.log(generatePassword);
+        }
+        return generatePassword;
+      }
+      //* If user choose a mix of carachters for his password from the 4 availables then the concat method will create a new arrey with the chosen mix
+      else {
+        if (upperCaseCharacthers)
+        {var options=upperCase}
+        if (lowerCaseCharacters)
+        {var options=options.concat(lowerCase)}
+        if (specialCharacters)
+        {var options=options.concat(special)}
+        if (numbersCharacters)
+        {var options=options.concat(numbers)}
+
+        var generatePassword=""
+        for (var i=0;i<passwordLenght;i++){
+          var mix = options[Math.floor(Math.random()*options.length)];
+          generatePassword=generatePassword+mix;
         }
         return generatePassword;
       }
